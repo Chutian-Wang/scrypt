8a443cf70d6cba1d0801cc73b35f3e49259e26c5
Evania Cheng <evaniacheng@169-231-173-226.wireless.ucsb.edu>
2023-10-12 16:40:38 -0700

    new

--- src/lib/lexer.cpp
+++ src/lib/lexer.cpp
@@ -1,10 +1,13 @@
-#include "token.h"
-#include "lexer.h"
+#include "lib/demo_lib.h"
+#include <iomanip>
+#include <vector>
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <cctype>
-using std::istringstream;
+#include "lib/token.h"
+#include "lib/lex.h"
 
 Lexer::Lexer() {
     currRow = 1;
@@ -17,8 +20,8 @@ Lexer::Lexer(const std::string &input) {
     this->tokenize(input);
 }
 
-void Lexer::addToken(TokenType type, const std::string &token){
-    tokens.push_back(Token(type, token, currRow, currCol));
+void Lexer::addToken(TokenType type, const std::string &token, int row, int col){
+    tokens.push_back(Token(type, token, row, col));
 }
 
 void Lexer::tokenize(const std::string& expr){
@@ -29,29 +32,50 @@ void Lexer::tokenize(const std::string& expr){
 
         if (std::isspace(token)){}
         else if (token == '('){
-            addToken(TokenType::Lparen, "(");
+            addToken(TokenType::Lparen, "(", currRow, currCol);
         }
         else if (token == ')'){
-            addToken(TokenType::Rparen, ")");
+            addToken(TokenType::Rparen, ")", currRow, currCol);
         }
         else if (token == '+'){
-            addToken(TokenType::Operator, "+");
+            addToken(TokenType::Operator, "+", currRow, currCol);
         }
         else if (token == '-'){
-            addToken(TokenType::Operator, "-");
+            addToken(TokenType::Operator, "-", currRow, currCol);
         }
         else if (token == '*'){
-            addToken(TokenType::Operator, "*");
+            addToken(TokenType::Operator, "*", currRow, currCol);
         }
         else if (token == '/'){
-            addToken(TokenType::Operator, "/");
+            addToken(TokenType::Operator, "/", currRow, currCol);
         }
-        else if (std::isdigit(token) || ((token == '.') && (std::isdigit(expr[currCol])))){
+        else if (std::isdigit(token) || (token == '.')){
             num += token;
-        }
-        else if (std::isdigit(token) || ((token == '.') && (!std::isdigit(expr[currCol])))){
-            num += token;
-            addToken(TokenType::Number, num);
+            if (std::isdigit(expr[currCol]) || (expr[currCol] == '.')){continue;}
+            else{
+                int n = std::count(num.begin(), num.end(), '.');
+                if (n == 0){
+                    addToken(TokenType::Number, num, currRow, currCol-(num.length()-1));
+                    num = "";
+                }
+                else if (n == 1){
+                    size_t idx = num.find('.');
+                    if ((idx == 0) || (idx == num.length()-1)){
+                        std::string str;
+                        str += token;
+                        throw Token(TokenType::Error, str, currRow, currCol);
+                    }
+                    else{
+                        addToken(TokenType::Number, num, currRow, currCol-(num.length()-1));
+                        num = "";
+                    }
+                }
+                else{
+                    std::string str;
+                    str += token;
+                    throw Token(TokenType::Error, str, currRow, currCol);
+                }
+            }
         }
         else if (token == '\n'){
             currRow ++;
@@ -62,7 +86,8 @@ void Lexer::tokenize(const std::string& expr){
             throw Token(TokenType::Error, str, currRow, currCol);
         }
     }
-    addToken(TokenType::END, "END");
+    currCol++;
+    addToken(TokenType::END, "END", currRow, currCol);
 }
 
 const std::vector<Token>& Lexer::getTokens() const{
@@ -73,4 +98,45 @@ Token::Token(TokenType type, const std::string &text, int row, int column): chr(
     this->type   = type;
     this->row    = row;
     this->column = column;
+}
+
+int main() {
+    // std::string line;
+    
+    // Lexer lexer;
+    
+    
+    // std::vector<Token> tokens;
+    // tokens = lexer.getTokens();
+
+    // for(auto token: tokens){
+    //     std::cout << std::setw(3) << token.row;
+    //     std::cout << std::setw(4) << token.column << "  ";
+    //     std::cout << token.chr;
+    // }
+
+    std::string line;
+    
+    //while(std::getline(std::cin, line));
+    std::getline(std::cin, line);
+    //std::cout << line << std::endl;
+    try{
+        Lexer lexer;
+        lexer.tokenize(line);
+
+        std::vector<Token> tokens;
+        tokens = lexer.getTokens();
+
+        for(auto token: tokens){
+            std::cout << std::setw(4) << token.row;
+            std::cout << std::setw(5) << token.column << "  ";
+            std::cout << token.chr << std::endl;
+        }
+    }
+    catch(Token error){
+        std::cout << "Syntax error on line " << error.row << " column " << error.column << ".\n";
+        exit(1);
+    }
+
+    return 0;
 }
