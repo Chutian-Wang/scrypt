55df71e51856adcf392dd02583c1d3c8e3cdb4f6
Chutian Wang <chutian_wang@ucsb.edu>
2023-10-12 09:51:17 -0700

    added more comments to lexer.h

--- src/lib/lexer.h
+++ src/lib/lexer.h
@@ -8,20 +8,29 @@
 class Lexer{
     public:
         Lexer(const std::string &input);
+        
         // Default constructor for empty instance creation
         Lexer();
 
-        // tokenize() is used to tokenize input S expression into tokens
-        // Tokens are then stored in vector tokens
+        // tokenize() is used to tokenize input S expression
+        // into tokens, which are then stored in vector tokens
+        // These tokens must be stored in **prefix order**,
+        // e.g. {(, +, (, *, 1, 2, ), 3, )} parsed from the S-
+        // expression (+ (* 1 2) 3) which evaluates to the
+        // mathematical expression 1 + 2 * 3
         void tokenize(const std::string& expr);
 
-        // getTokens() used to fetch the private member tokens 
         const std::vector<Token>& getTokens();
 
     private:
         std::vector<Token> tokens;
         int                currRow;
         int                currCol;
+
+        // If we must store raw text do it here
+        // Each element of this vector contains a line of raw
+        // text
+        std::vector<std::string> textLines;
 
         // Helper function
         // Used in tokenize() to add a new Token into the vector tokens 
