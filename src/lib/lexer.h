8e9d2f3f132bb18198d3df280e6f5cbb36944ea2
Jenniebn <jingyi_wu@umail.ucsb.edu>
2023-10-11 22:17:19 -0700

    oct 11 new PR

--- src/lib/lexer.h
+++ src/lib/lexer.h
@@ -1,23 +1,27 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#include <iostream>
 #include <vector>
 #include <string>
 
 class Lexer{
     public:
         Lexer(std::string &input);
-        void tokenize();
+        // tokenize() is used to tokenize input S expression into tokens
+        // Tokens are then stored in vector tokens
+        void               tokenize();
+        // getTokens() used to fetch the private member tokens 
         std::vector<Token> getTokens();
 
     private:
-        std::string &text;
+        std::string        &text;
         std::vector<Token> tokens;
-        int currRow;
-        int currCol;
+        int                currRow;
+        int                currCol;
 
         // Helper function
+        // Used in tokenize() to add a new Token into the vector tokens 
+        // each time a new token is reached
         void addToken(TokenType type, const std::string &token);
 };
 
