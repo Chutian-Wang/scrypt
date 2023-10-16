be425defabb7263974a8b8264e2e2c3139045f27
Chutian Wang <chutian_wang@ucsb.edu>
2023-10-15 23:11:55 -0700

    reveted accidental deletions in lex.cpp

--- src/lex.cpp
+++ src/lex.cpp
@@ -3,17 +3,22 @@
 #include "lib/Lexer.h"
 
 int main() {
+    try{
+        Lexer lexer;
+        lexer.tokenize(std::cin);
 
-    Lexer lexer;
-    lexer.tokenize(std::cin);
+        std::vector<Token> tokens = lexer.get_tokens();
 
-    std::vector<Token> tokens = lexer.get_tokens();
-
-    for(auto token: tokens){
-        std::cout << std::setw(4) << token.row;
-        std::cout << std::setw(5) << token.column << "  ";
-        std::cout << token.text << std::endl;
+        for(auto token: tokens){
+            std::cout << std::setw(4) << token.row;
+            std::cout << std::setw(5) << token.column << "  ";
+            std::cout << token.text << std::endl;
+        }
     }
-    
+    catch(Token error){
+        std::cout << "Syntax error on line " << error.row << " column " << error.column << ".\n";
+        exit(1);
+    }
+
     return 0;
 }
