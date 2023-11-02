9145429c99813b1bfa6cafb2718139753230afe9
Chutian Wang <chutian_wang@ucsb.edu>
2023-11-02 16:38:55 -0700

    CKPT 3 thread B starting point

---
+++ src/lib/Statement.h
@@ -0,0 +1,21 @@
+#include <vector>
+
+#include "AST.h"
+
+class Block {
+}
+
+class Statement {
+}
+
+class IfStatement : public Statement {
+}
+
+class ElseStatement : public Statement {
+}
+
+class WhileStatement : public Statement {
+}
+
+class PrintStatement : public Statement {
+}
