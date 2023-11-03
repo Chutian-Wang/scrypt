0a59a15097f285778a55a08120e0cfb469359e87
Chutian Wang <chutian_wang@ucsb.edu>
2023-11-03 11:58:30 -0700

    added more statement decleartions

--- src/lib/Statement.h
+++ src/lib/Statement.h
@@ -1,79 +1,108 @@
-#include <vector>
+#include <iostream>
 #include <memory>
+#include <vector>
 
 #include "AST.h"
 
+// A collection of statements which can be run as a whole
+// An entire scrypt program can and should be a block
 class Block {
-private:
-    std::vector<std::shared_ptr<Statement>> statements;
+ private:
+  std::vector<std::unique_ptr<Statement>> statements;
 
-public:
-    Block();
-    virtual ~Block();
+ public:
+  Block();
+  ~Block();
 
-    virtual void run();
+  void add_statement(std::unique_ptr<Statement>);
+  void run();
+  void print(std::ostream& os);
 };
 
+// Statement virtual base class
 class Statement {
-public:
-    Statement();
-    virtual ~Statement();
+ public:
+  Statement(){};
+  virtual ~Statement();
 
-    static Block parse_block(const std::vector<Token> &tokens, 
-                             std::vector<Token>::const_iterator &head);
-    virtual void run();
-    virtual bool syntax_valid() = 0;
+  static std::unique_ptr<Block> parse_block(
+      const std::vector<Token>& tokens,
+      std::vector<Token>::const_iterator& head);
+  virtual bool syntax_valid() = 0;
+  // This function runs  the statement and will
+  // print something if it contains a print statement
+  virtual void run() = 0;
+  // This function prints the statement itself
+  virtual void print(std::ostream) = 0;
 };
 
+// A single expression as a statement
 class Expression : public Statement {
-public:
-    Expression();
-    virtual ~Expression();
+ private:
+  std::shared_ptr<AST> expr;
 
-    std::shared_ptr<AST> expr;
+ public:
+  Expression(std::unique_ptr<AST> expr);
+  ~Expression();
 
-    virtual bool syntax_valid() override;
+  virtual bool syntax_valid();
+  virtual void run();
+  virtual void print(std::ostream& os);
 };
 
 class IfStatement : public Statement {
-public:
-    IfStatement();
-    virtual ~IfStatement();
+ private:
+  std::unique_ptr<Expression> condition;
+  std::unique_ptr<Block> if_block;
+  // If an else block does not exist this pointer is null
+  std::unique_ptr<Block> else_block;
 
-    Expression condExpr;
-    Block blockA;
-    Block blockB;
+ public:
+  IfStatement();
+  virtual ~IfStatement();
 
-    virtual bool syntax_valid();
-};
+  virtual bool syntax_valid();
+  virtual void run();
+  virtual void print(std::ostream& os);
 
-class ElseStatement : public Statement {
-public:
-    ElseStatement();
-    virtual ~ElseStatement();
-
-    Block eBlock;
-
-    virtual bool syntax_valid();
+  void set_cond(std::unique_ptr<Expression>);
+  void set_if(std::unique_ptr<Block>);
+  void set_else(std::unique_ptr<Block>);
 };
 
 class WhileStatement : public Statement {
-public:
-    WhileStatement();
-    virtual ~WhileStatement();
+ private:
+  std::unique_ptr<Expression> condition;
+  std::unique_ptr<Block> while_block;
 
-    Expression condExpr;
-    Block wBlock;
+ public:
+  WhileStatement();
+  virtual ~WhileStatement();
 
-    virtual bool syntax_valid();
+  virtual bool syntax_valid();
+  virtual void run();
+  virtual void print(std::ostream& os);
+
+  void set_cond(std::unique_ptr<Expression>);
+  void set_while(std::unique_ptr<Block>);
 };
 
 class PrintStatement : public Statement {
-public:
-    PrintStatement();
-    virtual ~PrintStatement();
+ private:
+  std::ostream& os;
+  std::unique_ptr<Expression> printee;
 
-    Expression printee;
+ public:
+  PrintStatement(std::unique_ptr<Expression> printee);
+  virtual ~PrintStatement();
 
-    virtual bool syntax_valid();
+  virtual bool syntax_valid();
+  // This function will push printee's evaluated
+  // value to this->os
+  virtual void run();
+  // This function will push the print expression
+  // to the passed in os, not this->os
+  virtual void print(std::ostream& os);
+
+  void set_ostream(std::ostream& os);
 };
