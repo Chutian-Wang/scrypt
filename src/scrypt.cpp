78a0e0b2fd7fbaa0f974ca5c3ac1e6f0a9e4cc18
Evania Cheng <evania@ucsb.edu>
2023-11-05 22:15:15 -0800

    format.cpp and scrypt.cpp

---
+++ src/scrypt.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "lib/AST.h"
+#include "lib/Errors.h"
+#include "lib/Lexer.h"
+#include "lib/Statement.h"
+
+int main() {
+  try {
+    Lexer lexer;
+    std::string input;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+      input += line + '\n';
+      std::istringstream iss(line);
+      lexer.tokenize(iss);
+    }
+
+    std::vector<Token> tokens = lexer.get_tokens();
+    std::vector<Token>::const_iterator head = tokens.begin();
+    std::unique_ptr<Block> program = Block::parse_block(tokens, head);
+
+    program->run();
+  } catch (const ScryptError &err) {
+    ScryptError::handle(std::cerr, err);
+    return 1;
+  }
+  return 0;
+}
