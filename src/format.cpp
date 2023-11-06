ddd4ff8a57d2b8c37c00a74f405525e07b7e7fca
Evania Cheng <evania@ucsb.edu>
2023-11-05 22:43:56 -0800

    testing output

--- src/format.cpp
+++ src/format.cpp
@@ -22,7 +22,12 @@ int main() {
     std::vector<Token>::const_iterator head = tokens.begin();
     std::unique_ptr<Block> program = Block::parse_block(tokens, head);
 
-    std::cout << program;
+    std::cout << "Token list: ";
+    for (const auto& token : tokens) {
+        std::cout << token.text << " ";
+    }
+    std::cout << std::endl;
+
     program->print(std::cout, 0);
   } catch (const ScryptError &err) {
     ScryptError::handle(std::cerr, err);
