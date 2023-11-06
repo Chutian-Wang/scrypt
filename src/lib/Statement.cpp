b785719a14d26b9d5ff805e7d4930e4d656a0e83
Evania Cheng <evania@ucsb.edu>
2023-11-06 14:52:59 -0800

    statement.cpp

--- src/lib/Statement.cpp
+++ src/lib/Statement.cpp
@@ -105,6 +105,7 @@ std::unique_ptr<Block> Block::parse_block(
   }
   if (head->type == TokenType::RCBRACE) head++;
   return block;
+  }
 }
 
 void Block::add_statement(std::unique_ptr<Statement> statement) {
