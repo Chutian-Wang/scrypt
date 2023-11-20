b1de1fe68fe37878ecb5c4c31ba5ffbe611c982f
Evania Cheng <evania@ucsb.edu>
2023-11-19 23:37:22 -0800

    std fix p2

--- src/lib/Function.cpp
+++ src/lib/Function.cpp
@@ -7,7 +7,7 @@
 
 Function::Function(std::weak_ptr<AST> functionAST) : funct(functionAST) {}
 
-std::map<std::__1::string, Value> Function::getScope() const {
+std::map<std::string, Value> Function::getScope() const {
   return my_scope;
 }
 
